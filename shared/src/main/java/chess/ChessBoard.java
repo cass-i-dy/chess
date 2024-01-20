4df29e33a4093e17969c53e608e4ef2c13bfe22b
cass-i-dy <120527008+cass-i-dy@users.noreply.github.com>
2024-01-20 00:40:13 -0700

    Added some starting code to ChessPiece

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -10,7 +10,7 @@ public class ChessBoard {
     private ChessPiece[][] board;
 
     public ChessBoard() {
-        board = new ChessPiece[8][8];
+        board = new ChessPiece[9][9];
     }
 
     /**
@@ -46,6 +46,6 @@ public class ChessBoard {
      */
     public void resetBoard() {
 //        throw new RuntimeException("Not implemented");
-        board = new ChessPiece[8][8];
+        board = new ChessPiece[9][9];
     }
 }
