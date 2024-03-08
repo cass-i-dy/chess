ed5b6631fea01047c01a4e5b9f90f8ee0470b975
cass-i-dy <120527008+cass-i-dy@users.noreply.github.com>
2024-03-07 22:00:15 -0700

    added files and some starter code

---
+++ server/src/main/java/service/RegisterService.java
@@ -0,0 +1,14 @@
+package service;
+import dataAccess.DataAccess;
+
+public class RegisterService {
+    private final DataAccess dataAccess;
+
+    public RegisterService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public void addUser(){
+
+    }
+}
